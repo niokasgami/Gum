7cbb70f9c29cd36e1e3d77ffe67065cfe200f9c5
Victor <VicChelaru@gmail.com>
2024-09-21 10:26:29 -0600

    Added grid splitter

---
+++ Gum2/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Gum2;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
