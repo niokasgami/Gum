2b89aba80b124819a22ddd93e2eed48325a3d6e3
Victor <VicChelaru@gmail.com>
2024-09-21 10:10:33 -0600

    First push of Gum2 Avalonia project

---
+++ Gum2/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Gum2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
